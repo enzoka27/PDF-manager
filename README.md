706a089e708e7c07cad3a54f7772856b3b01e5a4
Enzo <enzobtsimoes@gmail.com>
2025-03-05 22:51:27 -0300

    comitando os arquivos

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PDF-manager
 Automates the process of searching for your PDF's folder and makes it easier to open the file you want.
+
+For the program to run correctly, you must create a folder (or use an existent one) to put in all your PDF files you want to manage. Copy the path and paste like the example in the code, put it inside the commas ("") just like the example, after doing this you can just run it using any python debbugger, and now you don't need to waste time searching for folders or PDF files you want to open.
---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+
+def list_pdfs(path):
+    n = 1
+    for i in os.listdir(path):
+        print(f'\033[31m{n}-{i.replace(".pdf", "")}\033[m')  # or i[:-4] to remove the .pdf
+        n += 1  # add numbers to the PDFs
+
+def open_pdf(path, pdf_index):
+    try:
+        if 1 <= pdf_index <= len(os.listdir(path)):
+            os.startfile(f'{path}\{os.listdir(path)[pdf_index-1]}')
+        else:
+            print('PDF not found! Try a valid number.')
+    except ValueError:
+        print('Type a valid number.')  # Try and Except to avoid errors from the user
+
+def main():
+    path = 'C:\\ExamplePath'  # using double bars to avoid syntax error
+    list_pdfs(path)
+
+    while True:
+        try:
+            pdf_index = int(input('Which PDF do you want to open? '))
+            open_pdf(path, pdf_index)
+            break
+        except ValueError:
+            print('Type a valid number!')
+
+if __name__ == "__main__":  # to avoid running the code when importing the module
+    main()
